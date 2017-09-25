cbd85b3a49c0b1fd54231b42ba3cc9df3a651741
mudroljub <mudroljub@gmail.com>
2017-09-25 12:55:04 +0200

    webpack komande i slicno

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
+import pica from './podaci/pica'
 import Sto from './klase/Sto'
+
+// inicijalizujemo objekte
 
 const sto1 = new Sto('Sto u uglu')
 const sto2 = new Sto('Separe')
-
 sto1.slobodan = false
+
+// probamo metode
 
 sto1.getMeni()
 sto1.dodajPice(1)
