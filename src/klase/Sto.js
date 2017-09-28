6a408ea19060949d04d18cb3c854a66e2f5d1381
nikola5rovic <31932805+nikola5rovic@users.noreply.github.com>
2017-09-28 13:27:20 +0200

    add-reset-metod

--- src/klase/Sto.js
+++ src/klase/Sto.js
@@ -1,5 +1,5 @@
 import pica from '../podaci/pica'
-const meni = document.getElementById('meni')
+const meniId = 'meni'
 
 export default class Sto {
   constructor(ime) {
@@ -7,6 +7,7 @@ export default class Sto {
     this.porudzbine = []
     this.aktivaPorudzbina = {}
     this.ime = ime
+    this.reset()
   }
 
   getMeni() {
@@ -21,27 +22,29 @@ export default class Sto {
     for (var i = 0; i < pica.length; i++) {
       const div = document.createElement('div')
       div.innerHTML = `
-        <p>${pica[i].naziv}: ${pica[i].cena}</p>
+        ${pica[i].naziv}: ${pica[i].cena}
       `
       div.dataset.sifra = pica[i].sifra
-      div.onclick = () => {
-        this.dodajPice(div.dataset.sifra)
-      }
-      meni.appendChild(div)
+      div.onclick = () => this.dodajPice(div.dataset.sifra)
+      document.getElementById(meniId).appendChild(div)
     }
   }
 
   zatvoriMeni() {
-    meni.innerHTML = ''
+    document.getElementById(meniId).innerHTML = ''
   }
 
+  //resetuje
   reset() {
-    // briše sva stanja, poziva getMeni i prikaziMeni
+    this.getMeni()
+    this.prikaziMeni()
+    this.aktivaPorudzbina = {}
+    console.log("Reset")
   }
 
   dodajPice(sifraPica) {
     console.log(this.ime, 'sifra pica', sifraPica)
     // dodaje pice na aktivaPorudzbina
-    // aktivaPorudzbina je asocijativni niz sifra: kolicina
+    // this.aktivaPorudzbina dodaj sifraPica
   }
 }
